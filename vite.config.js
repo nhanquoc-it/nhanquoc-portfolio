fbc42dd36fdfe42c63645594cb1f161208f6c7cb
nhanquoc-it <caphuunhanquoc2510@gmail.com>
2025-05-17 17:25:13 +0700

    2. TailwindCSS Setup and Theme Customization Style CSS

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Lấy __dirname trong ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react(), tailwindcss()],
+
+	// Cấu hình import component > Sử dụng "@" thay vì "../../"
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
