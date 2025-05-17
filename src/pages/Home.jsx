1bdd2498148cfe33ef106ab766049e45377d4ca6
nhanquoc-it <caphuunhanquoc2510@gmail.com>
2025-05-17 22:01:11 +0700

    5. Create Hero Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import StarBackground from "@/components/StarBackground";
 import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
 
 const Home = () => {
 	return (
@@ -13,7 +14,9 @@ const Home = () => {
 			{/* Navbar */}
 			<Navbar />
 			{/* Main Content */}
-
+			<main>
+				<HeroSection />
+			</main>
 			{/* Footer */}
 		</div>
 	);
