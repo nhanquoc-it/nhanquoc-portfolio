0f981df88a8322fb98424ffa61b53e036a1cee41
nhanquoc-it <caphuunhanquoc2510@gmail.com>
2025-05-17 21:40:52 +0700

    4. Create Navbar Responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import StarBackground from "@/components/StarBackground";
+import Navbar from "@/components/Navbar";
 
 const Home = () => {
 	return (
@@ -10,7 +11,7 @@ const Home = () => {
 			{/* Background Effects */}
 			<StarBackground />
 			{/* Navbar */}
-
+			<Navbar />
 			{/* Main Content */}
 
 			{/* Footer */}
