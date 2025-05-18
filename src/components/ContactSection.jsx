7c24dd0da2f68f5e0ea4c8d91b46c188095759fe
nhanquoc-it <caphuunhanquoc2510@gmail.com>
2025-05-18 22:01:59 +0700

    11. Fix bugs - ContactSection

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -33,7 +33,7 @@ const ContactSection = () => {
 	return (
 		<section id="contact" className="px-24 px-4 relative bg-secondary/30">
 			<div className="container mx-auto max-w-5xl">
-				<h2 className="text-3xl md:text-4xl font-bold mb-44 text-center">
+				<h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
 					Get In <span className="text-primary"> Touch</span>
 				</h2>
 
